e6534d55473fc492acf44998b0365357d6733e1c
IGNACIO ANDRES DIAZ ROMERO <6003020@viewnext.com>
2024-04-29 14:39:37 +0200

    Rebotes y barra listos -> faltan ladrillos

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ canvas.width = 488;
 canvas.height = 400;
 
 //-------------------------Variables de la pelota------------------------------------
-const ballRadius = 3;
+const ballRadius = 5
 //posi pelota en mitad del tablero y abajo del todo -30
 let x = canvas.width / 2;
 let y = canvas.height - 30;
@@ -13,17 +13,17 @@ let dx = 4;
 let dy = -4;
 
 //-----------------------------Variables de la paleta--------------------------------
-const paddleWidth = 70;
-const paddleHeight= 10;
-const speedMove= 5;
+const paddleWidth = 100;
+const paddleHeight = 31;
+const speedMove = 5;
 //colocamos el paddle en la mitad restandole al canvas la anchura y haciendo su mitad
-let paddleX = (canvas.width - paddleWidth) /2
+let paddleX = (canvas.width - paddleWidth) / 2;
 //colocamos el paddle en su posicion por encima del final
-let paddleY = canvas.height - paddleHeight -15;
-let rightPressed =false;
-let leftPressed =false;
+let paddleY = canvas.height - paddleHeight - 10;
+let rightPressed = false;
+let leftPressed = false;
 
-//-----------------------------funcionalidad------------------------------------------
+//-----------------------------funcionalidades------------------------------------------
 function drawBall() {
   ctx.beginPath();
   //posiciones de incicio, arqueo, duracion del arco
@@ -34,9 +34,11 @@ function drawBall() {
 }
 
 function drawPaddle() {
-    ctx.fillStyle = "#red";
-    //coord x, coord y, ancho, alto
-    ctx.fillRect(paddleX, paddleY, paddleWidth, paddleHeight)
+ let img=new Image();
+ img.src="./paddle.png"
+  ctx.fillStyle = "#red";
+  //coord x, coord y, ancho, alto
+  ctx.drawImage(img, paddleX, paddleY , paddleWidth, paddleHeight);
 }
 
 function drawBricks() {}
@@ -61,57 +63,56 @@ function ballMove() {
   }
   //   pared inferior-> si la posicion mas el movimiento es mayor que la altura del canvas mas
   //   el radio de la pelota o toca la pala
-  const ballContactPaddleAtX = x > paddleX && x < paddleX + paddleWidth //contacto en eje x
-  
-  if(
-    false
-  ){
-    dy -dy;
-  }else if (y + dy > canvas.height - ballRadius) {
+
+  const ballContactPaddleAtX = x > paddleX && x < paddleX + paddleWidth; //contacto en eje x
+  const ballContactPaddleAtY = y > paddleY; //contacto en eje x
+  //si contacta en ambos cambiamos direccion si no hay game over
+  if (ballContactPaddleAtY && ballContactPaddleAtX) {
+    dy= - dy;
+  } else if (y + dy > canvas.height - paddleHeight) {
     console.log("Game Over MADAFAKA");
-    document.location.reload()
-   
+    document.location.reload();
   }
 }
 function paddleMove() {
-    //movemos y limitamos el movimiento al borde del canvas
-    if(rightPressed && paddleX<canvas.width - paddleWidth){
-        paddleX += speedMove;
-    }else if(leftPressed && paddleX>0){
-        paddleX -=speedMove
-    }
+  //movemos y limitamos el movimiento al borde del canvas
+  if (rightPressed && paddleX < canvas.width - paddleWidth +10) {
+    paddleX += speedMove;
+  } else if (leftPressed && paddleX > -10) {
+    paddleX -= speedMove;
+  }
 }
 function cleanCanvas() {
   //limpiamos lo dibujado para que quede como que actuliza al borrar lo anterior
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
-function initEvents(){
-    document.addEventListener("keydown" , keyDownHandler)
-    document.addEventListener("keyup" , keyUpHandler)
-    //en caso de que se de el evento no vamos a pinta aqui solo actualizamos variables
-    function keyDownHandler(ev){
-        const {key} = ev;
-        /*realizamos el if-elseif para que no reciba dos respuestas seguidas y solo tome
+function initEvents() {
+  document.addEventListener("keydown", keyDownHandler);
+  document.addEventListener("keyup", keyUpHandler);
+  //en caso de que se de el evento no vamos a pinta aqui solo actualizamos variables
+  function keyDownHandler(ev) {
+    const { key } = ev;
+    /*realizamos el if-elseif para que no reciba dos respuestas seguidas y solo tome
         una direccion*/
-        if(key==='right' || key==='ArrowRight'){
-            rightPressed=true;
-        }else if(key=='left' || key==='ArrowLeft'){
-            leftPressed=true;
-        }
+    if (key === "right" || key === "ArrowRight") {
+      rightPressed = true;
+    } else if (key == "left" || key === "ArrowLeft") {
+      leftPressed = true;
     }
-    function keyUpHandler(ev){
-        const {key} = ev;
-        /*realizamos el if-elseif para que no reciba dos respuestas seguidas y solo tome
+  }
+  function keyUpHandler(ev) {
+    const { key } = ev;
+    /*realizamos el if-elseif para que no reciba dos respuestas seguidas y solo tome
         una direccion*/
-        if(key==='right' || key==='ArrowRight'){
-            rightPressed=false;
-        }else if(key=='left' || key==='ArrowLeft'){
-            leftPressed=false;
-        }
+    if (key === "right" || key === "ArrowRight") {
+      rightPressed = false;
+    } else if (key == "left" || key === "ArrowLeft") {
+      leftPressed = false;
     }
+  }
 }
 function drawCanvas() {
-    console.log("LEFT ->"  + leftPressed + "RIGHT ->" + rightPressed );
+  console.log("LEFT ->" + leftPressed + "RIGHT ->" + rightPressed);
   cleanCanvas();
   //funcion para realizar los cambios en pantalla llamandose a si mismo frame por frame
   //hay que dibujar los elementos
